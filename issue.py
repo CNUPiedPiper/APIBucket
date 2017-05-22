1cee0f268c23eaba3a82bb95449fd210699ce45d
SuminJegal <geniusry@naver.com>
2017-05-22 20:37:52 +0900

    Modify issue.py

--- issue.py
+++ issue.py
@@ -2,15 +2,19 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url = urllib2.urlopen('http://datalab.naver.com/keyword/realtimeList.naver?where=main')
-soup = BeautifulSoup(url, 'html.parser')
+def get_issue():
+    url = urllib2.urlopen('http://datalab.naver.com/keyword/realtimeList.naver?where=main')
+    soup = BeautifulSoup(url, 'html.parser')
+    result = soup.find_all('ul')
 
-result = soup.find_all('ul')
+    issue_data = list()
 
-for res in result:
-    if res['class'][0] == 'rank_list':
-        keywords = res.find_all('span')
-        for key in keywords:
-            print key.contents[0]
-        break
+    for res in result:
+        if res['class'][0] == 'rank_list':
+            keywords = res.find_all('span')
+            for key in keywords:
+                issue_data.append(key.contents[0])
+            break
+
+    return issue_data
 
