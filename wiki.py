2b14e7f3c498c20cdbcbc4ca88e16cacdcc693fe
SuminJegal <geniusry@naver.com>
2017-09-20 13:40:44 +0900

    Upload wiki.py

--- wiki.py
+++ wiki.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*- 
 import wikipedia as wk
 import sys
 
@@ -5,12 +6,13 @@ def get_wiki(title):
     wk.set_lang('ko')
     try: 
         summary = wk.summary(title, sentences=2)
-    except wikipedia.exceptions.DisambiguationError as ed:
-        return ed.options
-    except wikipedia.exceptions.PageError as ep: 
-        retrun u'해당 정보가 없습니다.'
+    except wk.exceptions.DisambiguationError as ed:
+        return wk.summary(ed.options[0], sentences=2)
+    except wk.exceptions.PageError as ep: 
+        return u'해당 정보가 없습니다.'
+        
     return summary
 
-if __name__== '__main__" : 
+if __name__== '__main__' : 
     print get_wiki(sys.argv[1])
 
